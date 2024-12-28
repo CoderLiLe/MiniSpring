d6d72a40927013c81540e0333bb58b88907d5d3b
lile <1250124009@qq.com>
2024-12-28 11:12:28 +0800

    feat:02创建简单的bean容器

---
+++ MySpring/src/test/java/com/lile/springframework/test/ApiTest.java
@@ -0,0 +1,24 @@
+package com.lile.springframework.test;
+
+import com.lile.springframework.BeanDefinition;
+import com.lile.springframework.BeanFactory;
+import com.lile.springframework.test.bean.UserService;
+import org.junit.Test;
+
+public class ApiTest {
+
+    @Test
+    public void testBeanFactory() {
+        // 1.初始化 BeanFactory
+        BeanFactory beanFactory = new BeanFactory();
+
+        // 2.注册bean
+        BeanDefinition beanDefinition = new BeanDefinition(new UserService());
+        beanFactory.registerBeanDefinition("userService", beanDefinition);
+
+        // 3.获取bean
+        UserService userService = (UserService) beanFactory.getBean("userService");
+        userService.queryUserInfo();
+    }
+
+}
